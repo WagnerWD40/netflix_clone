e2b329c37bf3d08af4205e7d48ef009e792252d5
WagnerWD40 <wagnerwd40@gmail.com>
2020-11-04 23:50:18 -0300

    added header

---
+++ src/containers/header.js
@@ -0,0 +1,16 @@
+import { Header } from "../components";
+import * as ROUTES from '../constants/routes';
+
+function HeaderContainer({ children }) {
+    return (
+        <Header>
+            <Header.Frame>
+                <Header.Logo to={ROUTES.HOME} alt="Netflix" src="/images/logo.svg" />
+                <Header.ButtonLink to={ROUTES.SIGN_IN} >Sign In</Header.ButtonLink>
+            </Header.Frame>
+            {children}
+        </Header>
+    );
+}
+
+export default HeaderContainer;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,23 @@
 import JumbotronContainer from '../containers/jumbotron';
 import FooterContainer from '../containers/footer';
 import FaqsContainer from '../containers/faqs';
+import HeaderContainer from '../containers/header';
+import { OptForm } from '../components';
 
 function Home() {
     return (
         <>
+            <HeaderContainer>
+                <OptForm>
+                    <OptForm.Input placeholder="Email address" />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>
+                        Ready to watch? Enter your email to create or restart your
+                        membership
+                    </OptForm.Text>
+                </OptForm>
+            </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
